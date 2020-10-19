9fbdf0ac1001a36a9348b523680b7bc2802626e7
Brad Fitzpatrick <bradfitz@tailscale.com>
2020-10-19 09:36:52 -0700

    winipcfg, embeddable-dll-service, wintrust: fix Go 1.15 checkptr violations
    Avoids "converted pointer straddles allocation" failures at runtime
    when building binaries in race mode with Go 1.15, which enables
    checkptr: https://golang.org/doc/go1.15#windows
    
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- version/wintrust/certificate_windows.go
+++ version/wintrust/certificate_windows.go
@@ -48,7 +48,9 @@ func ExtractCertificates(path string) ([]x509.Certificate, error) {
 			break
 		}
 		buf := make([]byte, cert.Length)
-		copy(buf, (*[1 << 20]byte)(unsafe.Pointer(cert.EncodedCert))[:])
+		for i := range buf {
+			buf[i] = *(*byte)(add(unsafe.Pointer(cert.EncodedCert), uintptr(i)))
+		}
 		if c, err := x509.ParseCertificate(buf); err == nil {
 			certs = append(certs, *c)
 		} else {
@@ -56,4 +58,9 @@ func ExtractCertificates(path string) ([]x509.Certificate, error) {
 		}
 	}
 	return certs, nil
+}
+
+// add is a copy of unsafe.Add (https://github.com/golang/go/issues/40481)
+func add(p unsafe.Pointer, x uintptr) unsafe.Pointer {
+	return unsafe.Pointer(uintptr(p) + x)
 }
